bc1a29274660aa0c322465f224ebcab7913319d1
Nathan Smith <nls999@icloud.com>
2026-04-14 22:01:47 +0100

    Removed unnecessary export statements

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,2 +1,2 @@
-export const consoleLog = logString => console.log(`RESOURCE_MONITOR: ${logString}`);
-export const consoleError = errorString => console.error(`RESOURCE_MONITOR: ${errorString}`);
+const consoleLog = logString => console.log(`RESOURCE_MONITOR: ${logString}`);
+const consoleError = errorString => console.error(`RESOURCE_MONITOR: ${errorString}`);
