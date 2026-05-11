27840a6d59587ade481710647569d1c3f1aeb8e5
Nathan Smith <nls999@icloud.com>
2026-05-11 21:34:53 +0100

    Removed commented out legacy code from board-monitor.html

--- public/js/client.js
+++ public/js/client.js
@@ -14,4 +14,20 @@ TrelloPowerUp.initialize({
       },
     }];
   },
+  'on-enable': function(t,options) {
+        return [{
+        text: 'Resource Monitor',
+        callback: function(t) {
+            return t.modal({
+            title: 'Authorize: Resource Monitor for Trello',
+            url: 'authorize.html',
+            fullscreen: false,
+            });
+        },
+        }];
+  }
 });
+
+const authorizePup = () => {
+
+}
