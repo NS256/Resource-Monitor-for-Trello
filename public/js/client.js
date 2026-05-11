1b46dba6a63939140272c401e9529c34392a1c89
Nathan Smith <nls999@icloud.com>
2026-05-11 22:20:15 +0100

    added alternative authorization flows for initially enabling the power-up to test

--- public/js/client.js
+++ public/js/client.js
@@ -14,20 +14,30 @@ TrelloPowerUp.initialize({
       },
     }];
   },
-  'on-enable': function(t,options) {
-        return [{
-        text: 'Resource Monitor',
-        callback: function(t) {
-            return t.modal({
-            title: 'Authorize: Resource Monitor for Trello',
-            url: 'authorize.html',
-            fullscreen: false,
-            });
-        },
-        }];
+  'authorization-status': function(t, options) {
+    // Check if the user is authorized
+    return t.getRestApi().isAuthorized().then(function(isAuthorized) {
+      return { authorized: isAuthorized };
+    });
+  },
+  'show-authorization': function(t, options) {
+    // Show authorization modal when user clicks "Authorize Account"
+    return t.modal({
+      title: 'Authorize: Resource Monitor for Trello',
+      url: 'authorize.html',
+      fullscreen: false,
+    });
+  },
+  'on-enable': function(t, options) {
+    // Fallback in case on-enable is called
+    return t.getRestApi().isAuthorized().then(function(isAuthorized) {
+      if (!isAuthorized) {
+        return t.modal({
+          title: 'Authorize: Resource Monitor for Trello',
+          url: 'authorize.html',
+          fullscreen: false,
+        });
+      }
+    });
   }
 });
-
-const authorizePup = () => {
-
-}
